f9769aadd314fff19d1bb77f03965cd9a53169c3
Adrián Mouly <adrian@mouly.com.ar>
2014-10-12 00:41:31 -0300

    Adding pins bucle.

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
+/*global require,console*/
 var http = require('http');
 var gpio = require("pi-gpio");
 
+var pins = [12, 16, 18, 22];
+var i;
 
-gpio.open(16, "output", function(err) {        // Open pin 16 for output
-    gpio.write(16, 1, function() {            // Set pin 16 high (1)
-        gpio.close(16);                        // Close pin 16
+function show(pin) {
+    'use strict';
+
+    gpio.open(pin, "output", function (err) {
+        // Set pin to high (1)
+        gpio.write(pin, 1, function () {
+            gpio.close(pin);
+        });
     });
+}
+
+pins.forEach(function (pin) {
+    'use strict';
+
+    show(pin);
 });
 
-gpio.read(16, function(err, value) {
-    if(err) throw err;
+gpio.read(16, function (err, value) {
+    'use strict';
+
+    if (err) { throw err; }
     console.log(value);    // The current state of the pin
 });
 
 
-http.createServer(function (request,response) {
+http.createServer(function (request, response) {
+    'use strict';
+
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World!\n');
-}).listen(8000)
+}).listen(8000);
 
 console.log('Web Server running at http://127.0.0.1:8000');
 
