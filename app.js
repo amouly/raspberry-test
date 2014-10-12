d6853a831ba4ddb25c7cf45f8ba9cf64531fd6e6
Adrián Mouly <adrian@mouly.com.ar>
2014-10-12 00:07:30 -0300

    Change pins.

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var http = require('http');
 var gpio = require("pi-gpio");
 
 
-gpio.open(16, "output", function(err) {        // Open pin 16 for output
-    gpio.write(16, 1, function() {            // Set pin 16 high (1)
-        gpio.close(16);                        // Close pin 16
+gpio.open(23, "output", function(err) {        // Open pin 16 for output
+    gpio.write(23, 1, function() {            // Set pin 16 high (1)
+        gpio.close(23);                        // Close pin 16
     });
 });
 
