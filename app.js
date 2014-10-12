6ae2a4634ae3a074498b743e14d9b005875bd1d7
Adrián Mouly <adrian@mouly.com.ar>
2014-10-11 23:56:35 -0300

    Fixing port.

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (request,response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World!\n');
-}).listen(80)
+}).listen(8000)
 
-console.log('Web Server running at http://127.0.0.1:80');
+console.log('Web Server running at http://127.0.0.1:8000');
